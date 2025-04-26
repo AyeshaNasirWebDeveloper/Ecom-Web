324a534fd94a65ab6189a39543c883dcd5191c44
AyeshaNasirWebDeveloper <ayeshanasir806@gmail.com>
2025-04-26 19:00:56 +0500

    frontend deployment

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,14 +5,18 @@ import { fileURLToPath, URL } from 'url'
 
 export default defineConfig({
   plugins: [react()],
+  base : "/",
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:8080', // backend url
+        target: 'https://e-commerce.railway.internal.app', 
         changeOrigin: true,
         secure: false,
       },
     },
+    headers: {
+      "Content-Type": "text/javascript"
+    }
   },
   resolve: {
     alias: {
