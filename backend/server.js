9fb723b4bb8f83771365a31577a4d4a8c6db5a1e
Ayesha Nasir <153952026+AyeshaNasirWebDeveloper@users.noreply.github.com>
2025-05-28 01:38:14 +0500

    Add backend url

--- backend/server.js
+++ backend/server.js
@@ -7,23 +7,16 @@ import connectDB from "./database/db.js";
 import authRoute from "./routes/authRoute.js";
 import categoryRoute from "./routes/categoryRoute.js";
 import productRoutes from "./routes/productRoute.js";
-// import path from "path";
 
 // database
 connectDB();
 
 // rest object
 const app = express();
+
+// Middleware
 app.use(cors());
 app.use(express.json());
-
-// middleware
-// app.use(cors({
-//   origin: 'http://localhost:5173'
-// }));
-
-// app.options('*', cors());
-
 app.use(morgan("dev"));
 
 // routes
@@ -31,20 +24,9 @@ app.use("/api/v1/auth", authRoute);
 app.use("/api/v1/category", categoryRoute);
 app.use("/api/v1/products", productRoutes);
 
-// production static files
-// app.use(express.static(path.join(path.resolve(), './frontend/dist')));
-// app.get('*', (req, res) => {
-//   res.sendFile(path.join(__dirname, 'frontend', 'dist', 'index.html'));
-// });
-
 app.get('/', (req, res) => {
   res.send('Backend is running successfully!' );
 });
-
-// app.use("/", (req, res, next) => {
-//   console.log("Request URL:", req.url, "method: ", req.method, 'Welcome server!');
-//   next();
-// });
 
 // server start
 const PORT = process.env.PORT || 3000;
